67f06a0108cc7bf8708bbb7e5996dd1aea28969c
Deepak <deepaknnayak34@gmail.com>
2023-07-03 17:41:04 +0530

    'updated

--- Component/component/src/App.js
+++ Component/component/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css'; 
 import {User} from './User';
+import ClassComp from './Classcomponent';
+
 
 
 function App() {
   return (
     <div className="App">
       <User/>
+      <ClassComp />
     </div>
   );
 }
