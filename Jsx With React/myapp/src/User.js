6fa20e9d0d4ba0e86d61cd2e5b5e201a3435ebb4
Deepak <deepaknnayak34@gmail.com>
2023-07-06 10:34:53 +0530

    update

--- Jsx With React/myapp/src/User.js
+++ Jsx With React/myapp/src/User.js
@@ -1,9 +1,30 @@
-function User(){
+// this is also right
+// the second one also right 
+// the diffrence between both those two function are 
+// we can directly write export default function functionName instead of writting it below.
+
+import React, { useState } from "react"
+
+
+// function User(){
+//     return(
+//         <div>
+//             <h2>Jsx Example</h2>
+//         </div>
+//     )
+// }
+
+// export default User;
+
+
+export default function User(){
+    
+
     return(
         <div>
             <h2>Jsx Example</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            
         </div>
-    )
+    )  
 }
-
-export default User;
