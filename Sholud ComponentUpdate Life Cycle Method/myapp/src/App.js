45661f6e8310c5b23d13bddaf9ec430babaf53a1
Deepak <deepaknnayak34@gmail.com>
2023-07-09 10:21:00 +0530

    Component Shoiuld Update Project

--- Sholud ComponentUpdate Life Cycle Method/myapp/src/App.js
+++ Sholud ComponentUpdate Life Cycle Method/myapp/src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      count:0
+    }
+  }
+
+  shouldComponentUpdate(){
+    console.warn(this.state.count)
+    if(this.state.count>0 && this.state.count<10){
+      return true
+    }
+    return false
+    
+    // False is the bydefault value it doesnt allow you to update
+    // if you want to update it then you have to write return true instead of 
+    // return false
+
+    // here i hae applied some condition we can also perfom operation like that 
+    // executinh with some random conditions.
+  }
+  render(){
+    return(
+      <div className="App">
+        <h2>Should Component Update</h2>
+        <p>Value of count : {this.state.count}</p>
+        <button className="Update" onClick = {()=> this.setState({count: this.state.count+1})}>Update Counter</button>
+      </div>
+    )
+  }
 }
 
 export default App;
+
+
+// Notes:- 
+
+// Should Component Update Mean its asks You a question that whether should i update the component or not.
+// It can stop Rendering 
+// used with state and props.
+
+// Bascially it comes during update. whenever will state and props will update at that time 
+// only it will run.
+
+// Question
+// Can we call an api inside componentDidUpdate
+// yes we can if we want to do conditional call then we can 
+
+// and if you dnt want to do conditonal call then we can do inside the 
+// componentDidMount
+
+
