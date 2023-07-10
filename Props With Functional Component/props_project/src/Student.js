488009890052ba1bb9db5fe6748316440031f5e1
Deepak <deepaknnayak34@gmail.com>
2023-07-10 17:04:59 +0530

    added some notes about props

--- Props With Functional Component/props_project/src/Student.js
+++ Props With Functional Component/props_project/src/Student.js
@@ -2,6 +2,10 @@ function Student(props){
     return(
         <div style={{backgroundColor: "#BEEFF5", width: "max-content", padding: 5, marginLeft: "auto", marginRight: "auto", marginBottom : 4}}>
             <h4>Students: {props.name}</h4>
+
+            {/* props.name = "deepak" this is not allowed we 
+            cannot change the value of props */}
+
             <p>Email: {props.email}</p>
             <p>Address : {props.address}</p>
         </div>
@@ -13,3 +17,8 @@ export default Student
 
 // function Student(props) : instead of props i can also used data its better to use props and if i  want to access it 
 // for that i have to write {props.name}
+
+// props are immutable we cannot change its value once it is assigned.
+
+// in class component we can access props by writting
+// {this.props.name}
