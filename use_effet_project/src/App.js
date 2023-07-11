2192678877c95faacb36912735aad5b68cfce0da
Deepak <deepaknnayak34@gmail.com>
2023-07-11 16:34:25 +0530

    code added useEffect

--- use_effet_project/src/App.js
+++ use_effet_project/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0)
+
+
+  useEffect(()=>{
+    consolee.warn('useEffect')
+  })
+
+  // useEffect(()=>{
+  //     console.log('This is will only on data2')
+  // },[data2])
+
+  // useEffect(()=>{
+  //    console.log('more than one useEffcet we can use') 
+  //    console.log('This usEffect will work on data')
+  // },[data])
+
+  // useEffect(()=>{
+      
+  // })
+  // so this is how we can use it according to to our need more than 
+  // one useEffect we can use
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <h1>Using useEffect without any condition</h1>
+      <h3>Count: {count}</h3>
+      <button  onClick = {() =>setCount(count+1)}>Change Count Value</button>
     </div>
   );
 }
