f36a5c5131834fa26a1c090da9a9e5e4ad0eacd0
Deepak <deepaknnayak34@gmail.com>
2023-07-16 17:43:14 +0530

    added NavLink

--- ReactRouter/router-params/src/Component/Navbar.js
+++ ReactRouter/router-params/src/Component/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
+
 
 function Navbar() {
   return (
     <>
     <ul className="Navbar">
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/user/deepak">Deepak</Link></li>
-        <li><Link to="/user/peter">Peter</Link></li>
-        <li><Link to="/page404">Page</Link></li>
+        <li><NavLink className = "navlink" to="/about">About</NavLink></li>
+        <li><NavLink className = "navlink" to="/home">Home</NavLink></li>
+        <li><NavLink className = "navlink" to="/contact">Contact</NavLink></li>
+        <li><NavLink className = "navlink" to="/user/deepak">Deepak</ NavLink></li>
+        <li><NavLink className = "navlink" to="/user/peter">Peter</ NavLink></li>
+        <li><NavLink className = "navlink" to="/page404">Page</ NavLink></li>
         
 
     </ul>
@@ -21,3 +22,16 @@ function Navbar() {
 }
 
 export default Navbar
+
+// instead of using Link  can also use Navlink
+// because Link doesnot allow you to add any class name
+// but NavLink Does. 
+
+{/* <Link className = "link" to="/about">About</Link> */}
+// we cannot do like this
+
+// But
+
+// <NavLink className = "navlink" to="/about">About</NavLink>
+// we can do like this and add some styles to it.
+
